cc58157a0b38e55c7e244f7a55860da5c6c36cb2
Camden Brown <camden.brown@metova.com>
2017-06-22 16:06:08 -0500

    convert index to ES6

---
+++ index.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function(robot, scripts) {
+  let scriptsPath = path.resolve(__dirname, 'src');
+  if (fs.existsSync(scriptsPath)) {
+    return (() => {
+      let result = [];
+      for (let script of Array.from(fs.readdirSync(scriptsPath).sort())) {
+        if ((scripts != null) && !Array.from(scripts).includes('*')) {
+          if (Array.from(scripts).includes(script)) { result.push(robot.loadFile(scriptsPath, script)); } else {
+            result.push(undefined);
+          }
+        } else {
+          result.push(robot.loadFile(scriptsPath, script));
+        }
+      }
+      return result;
+    })();
+  }
+};
